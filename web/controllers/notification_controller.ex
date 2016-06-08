66021f457fd68ee6e5584d329e193bf3393637bc
Tyler Willingham <tylerwillingham@gmail.com>
2016-06-08 11:03:57 -0500

    Actually, use html generator

--- web/controllers/notification_controller.ex
+++ web/controllers/notification_controller.ex
@@ -1,7 +1,67 @@
 defmodule Echo.NotificationController do
   use Echo.Web, :controller
 
+  alias Echo.Notification
+
+  plug :scrub_params, "notification" when action in [:create, :update]
+
   def index(conn, _params) do
-    render conn, notifications: %{:body => "Hi there, I'm a notification"}
+    notifications = Repo.all(Notification)
+    render(conn, "index.html", notifications: notifications)
+  end
+
+  def new(conn, _params) do
+    changeset = Notification.changeset(%Notification{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"notification" => notification_params}) do
+    changeset = Notification.changeset(%Notification{}, notification_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _notification} ->
+        conn
+        |> put_flash(:info, "Notification created successfully.")
+        |> redirect(to: notification_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    notification = Repo.get!(Notification, id)
+    render(conn, "show.html", notification: notification)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    notification = Repo.get!(Notification, id)
+    changeset = Notification.changeset(notification)
+    render(conn, "edit.html", notification: notification, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "notification" => notification_params}) do
+    notification = Repo.get!(Notification, id)
+    changeset = Notification.changeset(notification, notification_params)
+
+    case Repo.update(changeset) do
+      {:ok, notification} ->
+        conn
+        |> put_flash(:info, "Notification updated successfully.")
+        |> redirect(to: notification_path(conn, :show, notification))
+      {:error, changeset} ->
+        render(conn, "edit.html", notification: notification, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    notification = Repo.get!(Notification, id)
+
+    # Here we use delete! (with a bang) because we expect
+    # it to always work (and if it does not, it will raise).
+    Repo.delete!(notification)
+
+    conn
+    |> put_flash(:info, "Notification deleted successfully.")
+    |> redirect(to: notification_path(conn, :index))
   end
 end
--- web/views/notification_view.ex
+++ web/views/notification_view.ex
@@ -1,7 +1,3 @@
 defmodule Echo.NotificationView do
   use Echo.Web, :view
-
-  def render("index.json", %{notifications: notifications}) do
-    notifications
-  end
 end
