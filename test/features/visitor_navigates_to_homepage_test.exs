39b0119fee41fcb5550c584d74752ce9fb6b6d78
Tyler Willingham <tylerwillingham@gmail.com>
2016-06-08 15:39:57 -0500

    Make that one feature test better

--- test/features/visitor_navigates_to_homepage_test.exs
+++ test/features/visitor_navigates_to_homepage_test.exs
@@ -2,11 +2,19 @@ defmodule VisitorNavigatesToHomepageTest do
   use ExUnit.Case
   use Hound.Helpers
 
+  alias Echo.Repo
+  alias Echo.Notification
+
   hound_session
 
   test "Notifications are listed" do
+    Repo.insert(%Notification{body: "Onomatopoeia"})
+    Repo.insert(%Notification{body: "Cool peas"})
+
     navigate_to("/")
 
     assert visible_page_text() =~ "Listing notifications"
+    assert visible_page_text() =~ "Cool peas"
+    assert visible_page_text() =~ "Onomatopoeia"
   end
 end
