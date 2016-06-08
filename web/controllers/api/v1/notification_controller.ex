49de11762f1a795082c2c9e042e0886567a72c3e
Ryan Troxler <rtroxler@rednovalabs.com>
2016-06-08 16:25:21 -0500

    Much better

--- web/controllers/api/v1/notification_controller.ex
+++ web/controllers/api/v1/notification_controller.ex
@@ -6,25 +6,29 @@ defmodule Echo.Api.V1.NotificationController do
   alias Echo.Notification
   alias Echo.Customer
 
-  plug :find_customer
+  plug :find_customer_or_halt
 
   def index(conn, %{"user_id" => user_id}) do
     Logger.debug "Loggin some text for user_id: #{user_id}."
+
+    # Debug stuffs
+    user_count = List.first(Repo.all(from c in Customer, select: count(c.id)))
+    Logger.debug "Current number of users: #{user_count}"
 
     notifications = Repo.all(Notification)
     render(conn, "index.json", notifications: notifications)
   end
 
-  defp find_customer(conn, _) do
+  defp find_customer_or_halt(conn, _) do
     case conn.params["user_id"] do
-      nil -> conn |> render_error
-      _   -> conn |> fetch_or_build_customer
+      nil     -> conn |> render_error
+      user_id -> conn |> fetch_or_build_customer(user_id)
     end
   end
 
-  defp fetch_or_build_customer(conn) do
-    customer = case Repo.get_by(Customer, app_user_id: conn.params["user_id"]) do
-      nil -> Repo.insert!(Customer.changeset(%Customer{}, %{ app_user_id: conn.params["user_id"] }))
+  defp fetch_or_build_customer(conn, user_id) do
+    customer = case Repo.get_by(Customer, app_user_id: user_id) do
+      nil -> Repo.insert!(Customer.changeset(%Customer{}, %{ app_user_id: user_id }))
       customer -> customer
     end
     assign(conn, :customer, customer)
