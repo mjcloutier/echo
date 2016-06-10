f75660df5fafbc9b8ef10a7d76da84ba61bf100a
Tyler Willingham <tylerwillingham@gmail.com>
2016-06-10 12:45:54 -0500

    Move Notification form echo types into view layer

--- web/views/notification_view.ex
+++ web/views/notification_view.ex
@@ -1,3 +1,15 @@
 defmodule Echo.NotificationView do
   use Echo.Web, :view
+
+  def application_types do
+    [{"FMS", "fms"}]
+  end
+
+  def echo_types do
+    [
+      {"Immediate", "immediate"},
+      {"Scheduled", "scheduled"},
+      {"Login count", "login-count"}
+    ]
+  end
 end
