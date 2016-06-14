2a56aba788433f5fe261b4d0e22c7f09651a63be
Tyler Willingham <tylerwillingham@gmail.com>
2016-06-14 16:23:59 -0500

    Introduce vanilla Application model

---
+++ priv/repo/migrations/20160614211617_create_application.exs
@@ -0,0 +1,12 @@
+defmodule Echo.Repo.Migrations.CreateApplication do
+  use Ecto.Migration
+
+  def change do
+    create table(:applications) do
+      add :name, :string
+
+      timestamps
+    end
+
+  end
+end
--- web/controllers/settings/application_controller.ex
+++ web/controllers/settings/application_controller.ex
@@ -1,11 +1,12 @@
 defmodule Echo.Settings.ApplicationController do
   use Echo.Web, :controller
 
+  alias Echo.Application
+
   def index(conn, _params) do
-    applications = [
-      %{name: "Something"},
-      %{name: "Something else"}
-    ]
+    applications = Repo.all(from a in Application,
+                            order_by: [desc: a.inserted_at])
+
     render conn, "index.html", applications: applications
   end
 end
