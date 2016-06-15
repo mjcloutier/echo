134cad897eef8b8674659e3195bf311aefd6155f
Tyler Willingham <tylerwillingham@gmail.com>
2016-06-15 15:51:23 -0500

    Remove fragile controller tests
    - These should be replaced in the future with UI tests if we deem this
      kind of testing valid

--- test/controllers/notification_controller_test.exs
+++ test/controllers/notification_controller_test.exs
@@ -20,46 +20,10 @@ defmodule Echo.NotificationControllerTest do
     assert html_response(conn, 200) =~ "New echo"
   end
 
-  test "creates resource and redirects when data is valid", %{conn: conn} do
-    request_params = Map.merge(%{type: "immediate", session_count: 0, start_at: nil}, @valid_attrs)
-
-    conn = post conn, notification_path(conn, :create), notification: request_params
-
-    assert redirected_to(conn) == notification_path(conn, :index)
-    assert Repo.get_by(Notification, @valid_attrs)
-  end
-
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    request_params = Map.merge(%{type: "immediate", session_count: 0, start_at: nil}, @invalid_attrs)
-
-    conn = post conn, notification_path(conn, :create), notification: request_params
-
-    assert html_response(conn, 200) =~ "New echo"
-  end
-
   test "renders form for editing chosen resource", %{conn: conn} do
     notification = Repo.insert! %Notification{}
 
     conn = get conn, notification_path(conn, :edit, notification)
-
-    assert html_response(conn, 200) =~ "Edit echo"
-  end
-
-  test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    request_params = Map.merge(%{type: "immediate", session_count: 0, start_at: nil}, @valid_attrs)
-    notification = Repo.insert! %Notification{}
-
-    conn = put conn, notification_path(conn, :update, notification), notification: request_params
-
-    assert redirected_to(conn) == notification_path(conn, :index)
-    assert Repo.get_by(Notification, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    request_params = Map.merge(%{type: "immediate", session_count: 0, start_at: nil}, @invalid_attrs)
-    notification = Repo.insert! %Notification{}
-
-    conn = put conn, notification_path(conn, :update, notification), notification: request_params
 
     assert html_response(conn, 200) =~ "Edit echo"
   end
