fa968c4e7a75eed3ac81ce093c48e835ec94799e
Ryan Troxler <rtroxler@rednovalabs.com>
2016-07-03 21:38:05 -0500

    Fix ack %

--- web/services/notification_statistics.ex
+++ web/services/notification_statistics.ex
@@ -8,7 +8,7 @@ defmodule Echo.NotificationStatistics do
       sent_count: sent_count(notification),
       seen_percent: (sent_count(notification) / echo_customer_count) * 100,
       acknowledged_count: acknowledged_count(notification),
-      acknowledged_percent: (acknowledged_count(notification) / echo_customer_count) * 100,
+      acknowledged_percent: (acknowledged_count(notification) / sent_count(notification)) * 100,
       total_customer_count: echo_customer_count
     }
   end
